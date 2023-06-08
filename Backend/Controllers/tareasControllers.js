71ef7e184acde70c215032b2799870268c6cf9f7
Mario Ramirez Cortez <isc.ramirez90@gmail.com>
2023-06-07 21:27:25 -0600

    Backend Finish

--- Backend/Controllers/tareasControllers.js
+++ Backend/Controllers/tareasControllers.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const tarea = require("../models/tareamodel");
 
 const getTareas = asyncHandler(async (req, res) => {
-   const Tarea = await tarea.find();
+   const Tarea = await tarea.find( {user: user.req.id} );
    res.status(200).json({ Tarea });
 });
 
@@ -14,6 +14,7 @@ const setTarea = asyncHandler(async (req, res) => {
    }
    const Tarea = await tarea.create({
       texto: req.body.texto,
+      user: req.user.id
    });
    res.status(201).json({ Tarea });
 });
@@ -26,21 +27,33 @@ const updateTarea =asyncHandler(async(req , res) =>{
         res.status(400);
         throw new Error("La Tarea no fue Encontrada");
     }
+
+    if (tarea.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Acceso no Autorizado')
+    }
+    
     const tareaUpdate = await tarea.findByIdAndUpdate(req.params.id, req.body, {new: true});
 
     res.status(200).json({ tareaUpdate });
 });
 
 const deleteTarea = asyncHandler(async (req, res) => {
-   const Tarea = await tarea.findById(req.params.id);
-   if (!Tarea) {
-      //res.status(400).json({message: 'Teclea una Tarea'});
-      res.status(400);
-      throw new Error("La Tarea no fue Encontrada");
-   }
-   await tarea.deleteOne();
+    if (!tarea) {
+        res.status(400)
+        throw new Error('La tarea no fué encontrada')
+    }
 
-   res.status(200).json({id: req.params.id});
+    if (tarea.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Acceso no Autorizado')
+    }
+
+    await tarea.deleteOne()
+
+    //await Tarea.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({ id: req.params.id })
 });
 
 module.exports = {
--- Backend/Routes/tareasRoutes.js
+++ Backend/Routes/tareasRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authMiddleware.js');m 
+const { protect } = require('../middleware/authMiddleware.js');
 const {getTareas , setTarea, updateTarea, deleteTarea} = require('../Controllers/tareasControllers.js');
 
-router.route('/').get(protect , getTareas).post(protect , setTarea);
+router.route('/').get(protect, getTareas).post(protect, setTarea)
 //router.get('/', getTarea);
 //router.post('/', setTarea);
 
-router.route('/:id').delete(protect , deleteTarea).put(protect , updateTarea);
+router.route('/:id').delete(protect, deleteTarea).put(protect, updateTarea);
 //router.put('/:id', updateTarea);
 //router.delete('/:id', deleteTarea)
 
